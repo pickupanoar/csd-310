b79cee0629a372667a4e8d84ddc01c6f3a66073d
pickupanoar <91435505+pickupanoar@users.noreply.github.com>
2022-06-25 16:55:22 -0400

    Delete mongodb_test.py

--- mongodb_test.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-url = "mongodb+srv://admin:<password>@cluster0.isljfkd.mongodb.net/?retryWrites=true&w=majority";
-client = MongoClient(url)
-db = client.pytech
-print( "--PyTech Collection List --" )
-print(db.list_collection_names())
