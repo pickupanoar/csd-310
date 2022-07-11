89ea2be8c131211821f7c2d547b39537c88154f3
pickupanoar <91435505+pickupanoar@users.noreply.github.com>
2022-07-10 21:51:24 -0400

    Update pysports_update_and_delete.py

--- module_9/pysports_update_and_delete.py
+++ module_9/pysports_update_and_delete.py
@@ -12,9 +12,7 @@ config = {
 
 
 def show_players(cursor, title):
-  
     cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
-
     players = cursor.fetchall()
 
     print("\n  -- {} --".format(title))
@@ -25,38 +23,25 @@ def show_players(cursor, title):
 try:
 
     db = mysql.connector.connect(**config) 
-
     cursor = db.cursor()
-
     add_player = ("INSERT INTO player(first_name, last_name, team_id)"
                  "VALUES(%s, %s, %s)")
 
     player_data = ("Jim", "Halpert", 1)
-
     cursor.execute(add_player, player_data)
-
     db.commit()
 
     show_players(cursor, "DISPLAYING PLAYERS AFTER INSERT")
-
     update_player = ("UPDATE player SET team_id = 2, first_name = 'Gollum', last_name = 'Ring Stealer' WHERE first_name = 'Smeagol'")
-
     cursor.execute(update_player)
 
     show_players(cursor, "DISPLAYING PLAYERS AFTER UPDATE")
-
     delete_player = ("DELETE FROM player WHERE first_name = 'Smeagol'")
-
     cursor.execute(delete_player)
-
     show_players(cursor, "DISPLAYING PLAYERS AFTER DELETE")
-    
-
     input("\n\n  Press any key to continue... ")
 
-except mysql.connector.Error as err:
-    """ handle errors """ 
-
+except mysql.connector.Error as err: 
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("  The supplied username or password are invalid")
 
@@ -67,6 +52,5 @@ except mysql.connector.Error as err:
         print(err)
 
 finally:
-    """ close the connection to MySQL """
 
     db.close()
